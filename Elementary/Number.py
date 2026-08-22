42736dfbf3c4e4c3a77528ec4fb844a189796d11
aleonevenerius <alexandreleonevenerius@gmail.com>
2026-08-22 17:52:35 -0300

    Commit i

---
+++ Elementary/Number.py
@@ -0,0 +1,12 @@
+# Check if a number is positive, negative, or zero.
+
+n = float(input("Number: "))
+
+if n > 0:
+    print("It is a positive number!")
+
+elif n < 0:
+    print("It is a negative number!")
+
+else:
+    print("It's a zero number.")
