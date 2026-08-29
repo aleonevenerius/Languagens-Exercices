0b10bd280ba1b4a53f97f3b0d5f88f4845e1baa5
aleonevenerius <alexandreleonevenerius@gmail.com>
2026-08-29 17:08:42 -0300

    New exercise

---
+++ Elementary/PositiveNegativeOrZero.py
@@ -0,0 +1,8 @@
+n = float(input("Please, inform me an number: "))
+
+if n > 0:
+    print(f"{n} is greater than zero. Then, is a positive number.")
+elif n < 0:
+    print(f"{n} is lower than zero. Then, is a negative number.")
+else:
+    print(f"{n} is zero.")
---
+++ Elementary/Rectangle.py
@@ -0,0 +1,3 @@
+b = float(input("Base value: "))
+h = float(input("Height value: "))
+print("The rectangle's area is ", b*h)
