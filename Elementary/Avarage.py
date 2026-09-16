15d87982d2b8d0dd637511616a573a81fb9d03c0
aleonevenerius <alexandreleonevenerius@gmail.com>
2026-09-15 21:20:52 -0300

    Avarage exercise

---
+++ Elementary/Avarage.py
@@ -0,0 +1,9 @@
+a, b, c = float(input("First value: ")), float(input("Second value: ")), float(input("Third value: "))
+avarage = (a+b+c)/3
+
+print(f"Your avarage is {avarage}")
+
+if avarage > 6:
+    print("Congratulations! You are aprove!")
+else:
+    print("Disapprove!")
